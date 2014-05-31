21aec3c4f4cf3b96c2e27e00a645ed064fe6419e
Maxim Polishchuck <mpolishchuck@ikto.net>
2014-05-31 14:01:43 +0300

    Added correct constants in PgI class

--- src/IKTO/PgI.php
+++ src/IKTO/PgI.php
@@ -3,6 +3,7 @@
 namespace IKTO;
 
 use IKTO\PgI\Database;
+use IKTO\PgI\ParamsEncoder;
 
 class PgI
 {
@@ -10,10 +11,10 @@ class PgI
     const TRANSACTION_ACTIVE        = 1;
     const TRANSACTION_ERROR         = 2;
 
-    const PARAM_BYTEA         = 'bytea';
-    const PARAM_JSON          = 'json';
-    const PARAM_TIMESTAMP     = 'timestamp';
-    const PARAM_TIMESTAMPTZ   = 'timestamptz';
+    const PARAM_BYTEA         = ParamsEncoder::BYTEA;
+    const PARAM_JSON          = ParamsEncoder::JSON;
+    const PARAM_TIMESTAMP     = ParamsEncoder::TIMESTAMP;
+    const PARAM_TIMESTAMPTZ   = ParamsEncoder::TIMESTAMPTZ;
 
     public static function connect($dsn, $user = null, $password = null)
     {
