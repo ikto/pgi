84d0a9729f5ee989facc0ebbeb39c82b76ba5895
Maxim Polishchuck <mpolishchuck@ikto.net>
2014-10-01 11:57:43 +0300

    Fixed bug in timestamp converters for compatibility with PHP < 5.5.0

--- src/IKTO/PgI/Converter/PgTimestamp.php
+++ src/IKTO/PgI/Converter/PgTimestamp.php
@@ -8,9 +8,7 @@ class PgTimestamp implements ConverterInterface
 {
     public function encode($value, $type = null)
     {
-        if (!($value instanceof \DateTimeInterface)) {
-            throw new InvalidArgumentException("The timestamp must be passed as instance of DateTimeInterface");
-        }
+        $this->assertValidValue($value);
 
         return $value->format('Y-m-d\TH:i:s');
     }
@@ -18,5 +16,18 @@ class PgTimestamp implements ConverterInterface
     public function decode($value, $type = null)
     {
         return new \DateTime($value);
+    }
+
+    /**
+     * Checks value validness
+     *
+     * @param mixed $value
+     */
+    protected function assertValidValue($value)
+    {
+        /* Also checks DateTime for PHP < 5.5.0 */
+        if (!($value instanceof \DateTime) && !($value instanceof \DateTimeInterface)) {
+            throw new InvalidArgumentException("The timestamp must be passed as instance of DateTime or DateTimeInterface");
+        }
     }
 }
--- src/IKTO/PgI/Converter/PgTimestampWithTimezone.php
+++ src/IKTO/PgI/Converter/PgTimestampWithTimezone.php
@@ -4,24 +4,24 @@ namespace IKTO\PgI\Converter;
 
 use IKTO\PgI\Exception\InvalidArgumentException;
 
-class PgTimestampWithTimezone implements ConverterInterface, EncoderGuesserInterface
+class PgTimestampWithTimezone extends PgTimestamp implements EncoderGuesserInterface
 {
     public function encode($value, $type = null)
     {
-        if (!($value instanceof \DateTimeInterface)) {
-            throw new InvalidArgumentException("The timestamp must be passed as instance of DateTimeInterface");
-        }
+        $this->assertValidValue($value);
 
         return $value->format(\DateTime::W3C);
     }
 
-    public function decode($value, $type = null)
-    {
-        return new \DateTime($value);
-    }
-
     public function canEncode($value)
     {
-        return ($value instanceof \DateTimeInterface);
+        try {
+            $this->assertValidValue($value);
+        }
+        catch (InvalidArgumentException $ex) {
+            return false;
+        }
+
+        return true;
     }
 }
